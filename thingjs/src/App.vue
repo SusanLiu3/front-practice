1affbff56d86b4ececbfb9daa289452103386d00
liushanshan <2498469106@qq.com>
2021-04-24 21:12:13 +0800

    feat:创建thingjs项目

---
+++ thingjs/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="div3d"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  mounted() {
+    /* eslint-disable */
+    const app = new THING.App({
+      container: "div3d", // 3D容器
+      url: 'https://www.thingjs.com/static/models/storehouse',
+    });
+  }
+};
+</script>
+
+<style>
+#app {
+}
+#div3d {
+  width: 100%;
+  height: 100%;
+}
+</style>
