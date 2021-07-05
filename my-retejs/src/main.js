ba9e897dc65f8db146fc52f6dd8ff357517aab13
liushanshan <liushanshan@uino.com>
2021-07-05 18:04:15 +0800

    feat:retejs 学习

---
+++ my-retejs/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import Rete from "rete";
+import ConnectionPlugin from 'rete-connection-plugin';
+import VueRenderPlugin from 'rete-vue-render-plugin';
+
+Vue.prototype.Rete = Rete;
+Vue.prototype.ConnectionPlugin = ConnectionPlugin
+Vue.prototype.VueRenderPlugin = VueRenderPlugin
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
