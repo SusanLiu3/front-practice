9d3dc98a9a0bccd02a21d9032959e8796d78075e
liushanshan <2498469106@qq.com>
2021-08-06 09:18:41 +0800

    feat:服务端渲染SSR webpack 打包

--- webpack-learn/project-one/webpack.config.js
+++ webpack-learn/project-one/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    filename: '[name].js'
   },
   devServer: {
     port:8081,
